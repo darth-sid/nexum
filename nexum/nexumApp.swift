2d88dd13e4c48fb4ca0d022a2a94aef89db72d79
darth-sid <55854690+darth-sid@users.noreply.github.com>
2022-05-14 19:18:12 -0700

    quicksave

--- nexum/nexumApp.swift
+++ nexum/nexumApp.swift
@@ -11,6 +11,25 @@ import SwiftUI
 let bgcolor = Color(red: 60/255, green: 60/255, blue: 60/255)
 let purple1 = Color(red: 88/255,green:0/255,blue:108/255)
 
+public struct Response: Codable {
+    var status: Int
+}
+
+func login(usr: String, pwd: String, authKey: String){
+    let payload: [String:String] = ["usr":usr,"pwd":pwd,"auth":authKey]
+    let payloadEncoded = try! JSONSerialization.data(withJSONObject: payload)
+    guard let url = URL(string:"http://192.168.1.23:5000/login" ) else {return}
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.httpBody = payloadEncoded
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let data = data {
+            if let decodedData = try? JSONDecoder().decode(Response.self, from: data) {
+                print("d:\(decodedData)")
+            }
+        }
+    }.resume()
+}
 
 //pages
 struct empty1: View {
@@ -132,6 +151,7 @@ struct Login: View {
     @State var usr = "";
     @State var pwd = "";
     @State var loggedIn = false;
+    @State var auth = "";
     var body: some View {
         if(loggedIn){
             Menu();
@@ -161,8 +181,23 @@ struct Login: View {
                             .multilineTextAlignment(.center)
                     }
                     Button(action:{
-                        loggedIn = true
-                        
+                        var i = 0;
+                        auth = ""
+                        while(i < 20){
+                            let x = Int.random(in: 0..<3)
+                            if (x==0){
+                                auth = auth + String(Int.random(in: 0..<10));
+                            }
+                            else if (x==1){
+                                auth = auth + String(UnicodeScalar(UInt8(Int.random(in: 65..<91))));
+                            }
+                            else{
+                                auth = auth + String(UnicodeScalar(UInt8(Int.random(in: 97..<123))));
+                            }
+                            i = i + 1;
+                        }
+                        print(auth)
+                        login(usr:usr,pwd:pwd,authKey:auth)
                     }){
                         //Text("Login")
                         Image(systemName:"chevron.right.circle").resizable().frame(width: 25.0, height: 25.0)
