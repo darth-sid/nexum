30cf9cf4fabc2bcbe70953cf9f7be773141906a8
Brighton Alcantara <brighton.alcantara@gmail.com>
2022-05-14 18:50:44 -0700

    remove deprecated code

--- nexum/nexumApp.swift
+++ nexum/nexumApp.swift
@@ -153,7 +153,6 @@ struct Profile: View {
                             }
                             }
                         Spacer()
-                            .frame()
                         }
                         Spacer()
                     }
